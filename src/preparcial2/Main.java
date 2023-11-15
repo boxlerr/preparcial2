39573bb47cf7db22c8cc3c6312794a7d3997c99a
Julian Boxler <julian.boxler@davinci.edu.ar>
2023-11-15 09:29:47 -0300

    Prueba cafe descuento

--- src/preparcial2/Main.java
+++ src/preparcial2/Main.java
@@ -6,7 +6,18 @@ class Main {
 
 		VentasDelDia ventasDelDia = new VentasDelDia();
 		
+
 		
+		Cafe cafeEjemplo = new Cafe("Latte", 1.50);
+
+		// Calcular el precio final sin tarjeta
+		double precioSinTarjeta = cafeEjemplo.calcularPrecioFinal(false);
+
+		// Calcular el precio final con tarjeta
+		double precioConTarjeta = cafeEjemplo.calcularPrecioFinal(true);
+
+		System.out.println("Precio sin tarjeta: $" + precioSinTarjeta);
+		System.out.println("Precio con tarjeta: $" + precioConTarjeta);
 	}
 
 }
