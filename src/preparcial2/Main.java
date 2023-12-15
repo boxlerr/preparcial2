c63bd40a82cc9535fb64bf7dd3a41c91cf57be36
boxlerr <boxlerjulian@hotmail.com>
2023-12-15 18:21:24 -0300

    asd
    asd

--- src/preparcial2/Main.java
+++ src/preparcial2/Main.java
@@ -4,24 +4,19 @@ import javax.swing.JOptionPane;
 class Main {
 
     public static void main(String[] args) {
-
-        VentasDelDia ventasDelDia = new VentasDelDia();
+    	VentasDelDia ventasDelDia = new VentasDelDia();
         EncargadoVenta empleado = new EncargadoVenta("Nombre", "123456", "admin", "admin123", "Caja 1");
-
         boolean inicioSesionExitoso = false;
         int intentos = 3;  // intentos
         do {
             String usuario = JOptionPane.showInputDialog("Ingrese el nombre de usuario:");
             String contrasena = JOptionPane.showInputDialog("Ingrese la contraseña:");
-
             inicioSesionExitoso = empleado.Login(usuario, contrasena);
-
             if (inicioSesionExitoso) {
                 JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso.");
                 int opcion;
 				do {
                    opcion = Cajero.mostrarMenu();
-
                     switch (opcion) {
                         case 1:
                             empleado.agregarVenta(ventasDelDia);
@@ -47,14 +42,11 @@ class Main {
                         default:
                             JOptionPane.showMessageDialog(null, "Opción no válida. Por favor, elija una opción del menú.");
                     }
-
                 } while (opcion != 7);
             } else {
                 JOptionPane.showMessageDialog(null, "Error al iniciar sesión. Intentos restantes: " + (--intentos));
             }
-
         } while (!inicioSesionExitoso && intentos > 0);
-
         if (!inicioSesionExitoso) {
             JOptionPane.showMessageDialog(null, "Número máximo de intentos alcanzado. La aplicación se cerrará.");
         }
